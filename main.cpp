df73491b74d64a294e45a532de68ecc89bf26094
PatriotCodes <patriot130690@gmail.com>
2018-09-06 11:27:40 +0300

    MSSIM algorythm

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+double getMSSIM( const Mat& i1, const Mat& i2)
+{
+    const double C1 = 6.5025, C2 = 58.5225;
+    /***************************** INITS **********************************/
+    int d     = CV_32F;
+
+    Mat I1, I2;
+    i1.convertTo(I1, d);           // cannot calculate on one byte large values
+    i2.convertTo(I2, d);
+
+    Mat I2_2   = I2.mul(I2);        // I2^2
+    Mat I1_2   = I1.mul(I1);        // I1^2
+    Mat I1_I2  = I1.mul(I2);        // I1 * I2
+
+    /*************************** END INITS **********************************/
+
+    Mat mu1, mu2;   // PRELIMINARY COMPUTING
+    GaussianBlur(I1, mu1, Size(11, 11), 1.5);
+    GaussianBlur(I2, mu2, Size(11, 11), 1.5);
+
+    Mat mu1_2   =   mu1.mul(mu1);
+    Mat mu2_2   =   mu2.mul(mu2);
+    Mat mu1_mu2 =   mu1.mul(mu2);
+
+    Mat sigma1_2, sigma2_2, sigma12;
+
+    GaussianBlur(I1_2, sigma1_2, Size(11, 11), 1.5);
+    sigma1_2 -= mu1_2;
+
+    GaussianBlur(I2_2, sigma2_2, Size(11, 11), 1.5);
+    sigma2_2 -= mu2_2;
+
+    GaussianBlur(I1_I2, sigma12, Size(11, 11), 1.5);
+    sigma12 -= mu1_mu2;
+
+    ///////////////////////////////// FORMULA ////////////////////////////////
+    Mat t1, t2, t3;
+
+    t1 = 2 * mu1_mu2 + C1;
+    t2 = 2 * sigma12 + C2;
+    t3 = t1.mul(t2);              // t3 = ((2*mu1_mu2 + C1).*(2*sigma12 + C2))
+
+    t1 = mu1_2 + mu2_2 + C1;
+    t2 = sigma1_2 + sigma2_2 + C2;
+    t1 = t1.mul(t2);               // t1 =((mu1_2 + mu2_2 + C1).*(sigma1_2 + sigma2_2 + C2))
+
+    Mat ssim_map;
+    divide(t3, t1, ssim_map);      // ssim_map =  t3./t1;
+
+    Scalar mssim = mean( ssim_map ); // mssim = average of ssim map
+    double result;
+    result = mssim.val[0];
+    return result;
+}
+
+int main( int argc, char** argv )
+{
+    String imageName( "sample.jpg" );
+    String imageName2( "dist-sample.jpg" );
+    Mat text(100,500,CV_8UC1);
+    if( argc > 1)
+    {
+        imageName = argv[1];
+    }
+    Mat image;
+    Mat image2;
+    image = imread( imageName, IMREAD_COLOR );
+    image2 = imread( imageName2, IMREAD_COLOR );  
+    if( image.empty() || image2.empty()) 
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+    putText(text, "Score: " + to_string(getMSSIM(image, image2)), Point(10, 40), FONT_HERSHEY_SIMPLEX, 1, Scalar(128));
+    imshow("result", text);
+    waitKey(0);
+    return 0;
+}
